925f597b8a1ec0d430fd774a9790f51032d3fb6a
恕安 賴 <107111212@mail.oit.edu.tw>
2021-03-30 15:17:17 +0800

    030

---
+++ 1HW/Prime.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _1HW
+{
+    public partial class Prime : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            int x = 97, a = 1, ans = 0;
+            for (a = 1; a <= x; a++) {
+                if ((x % a) == 0) {
+                    ans++;
+                }
+            }
+            if (ans == 2) {
+                Console.Write(x + "是質數");
+            }
+            else
+            {
+                Console.Write(x + "不是質數");
+            }
+        }
+    }
+}
